774e5001ed9e3fbf09918d7a84cb1ae51038f73f
Michael Booth <michael@databooth.com.au>
2023-07-14 12:19:05 +1000

    Black 120 formatted

--- docs/provided/simple_button_10.py
+++ docs/provided/simple_button_10.py
@@ -5,11 +5,12 @@ import pyfirmata
 import time
 
 contents = []
-contents.append( [ "Trial", "\tSide", "\tCorrect", "\tCorrections", "\tLatency"] )
+contents.append(["Trial", "\tSide", "\tCorrect", "\tCorrections", "\tLatency"])
 
-board = pyfirmata.Arduino('/dev/ttyACM0')
-led = board.get_pin('d:10:o')
-pin = board.get_pin('a:1:i')
+board = pyfirmata.Arduino("/dev/ttyACM0")
+led = board.get_pin("d:10:o")
+pin = board.get_pin("a:1:i")
+
 
 def set_up_sensor():
     iterator = pyfirmata.util.Iterator(board)
@@ -18,17 +19,19 @@ def set_up_sensor():
     pin.mode = pyfirmata.INPUT
     pin.enable_reporting()
 
-def switch_arduino_on(): # turns on the red LED on Arduino board for 1 second
+
+def switch_arduino_on():  # turns on the red LED on Arduino board for 1 second
     led.write(1)
     time.sleep(1)
     led.write(0)
 
+
 WIDTH = 1850
 HEIGHT = 1000
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))   # ,  pygame.FULLSCREEN
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))  # ,  pygame.FULLSCREEN
 pygame.display.set_caption("Cognitive Test - Two Choice Discrimination Task")
 
-WHITE = (255,255,255)
+WHITE = (255, 255, 255)
 
 FPS = 60
 
@@ -38,24 +41,27 @@ correct_image_scaled = pygame.transform.scale(correct_image, (875, 650))
 wrong_image = pygame.image.load("wrong.jpg")
 wrong_image_scaled = pygame.transform.scale(wrong_image, (875, 650))
 
+
 def draw_window(left_image, right_image):
     WIN.fill(WHITE)
-    WIN.blit(left_image, (30,150))
-    WIN.blit(right_image, (950,150))
+    WIN.blit(left_image, (30, 150))
+    WIN.blit(right_image, (950, 150))
     pygame.display.update()
+
 
 def generate_stimuli():
     # cross reference with the trail number so it knows which part of the array to check - display correct image on the left if stimuli_left[trial] == 1
-    rm=[ [1,1,0,1,1,0,0,1,0,0],
-         [1,0,1,0,1,1,0,0,1,0],
-         [1,0,0,1,0,1,0,1,0,1],
-         [0,0,1,0,1,1,0,0,1,1],
-         [0,0,1,1,0,1,0,1,1,0],
-         [0,1,1,0,0,1,0,1,1,0],
-         [0,1,0,1,1,0,0,1,0,1],
-         [0,1,1,0,0,1,0,1,0,1],
-         [1,1,0,0,1,0,1,0,1,0],
-         [1,0,0,1,0,1,0,1,0,1]
+    rm = [
+        [1, 1, 0, 1, 1, 0, 0, 1, 0, 0],
+        [1, 0, 1, 0, 1, 1, 0, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
+        [0, 0, 1, 0, 1, 1, 0, 0, 1, 1],
+        [0, 0, 1, 1, 0, 1, 0, 1, 1, 0],
+        [0, 1, 1, 0, 0, 1, 0, 1, 1, 0],
+        [0, 1, 0, 1, 1, 0, 0, 1, 0, 1],
+        [0, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+        [1, 1, 0, 0, 1, 0, 1, 0, 1, 0],
+        [1, 0, 0, 1, 0, 1, 0, 1, 0, 1],
     ]
 
     stimuli_left = random.choice(rm)
@@ -63,7 +69,6 @@ def generate_stimuli():
 
 
 def main():
-
     set_up_sensor()
 
     # reading when object is detected = { 0.1574, 0.1584  }
@@ -72,7 +77,7 @@ def main():
     object_undetected = True
     while object_undetected:
         value = pin.read()
-        if ( value == 0.1574 or value == 0.1584):
+        if value == 0.1574 or value == 0.1584:
             object_undetected = False
             latency_start_time = time.time()
         time.sleep(0.1)
@@ -85,46 +90,54 @@ def main():
     run = True
     while run:
         clock.tick(FPS)
-        if stimuli[trial] == 1: # display correct image on left side
+        if stimuli[trial] == 1:  # display correct image on left side
             draw_window(correct_image_scaled, wrong_image_scaled)
-        else: # display wrong image on left side
+        else:  # display wrong image on left side
             draw_window(wrong_image_scaled, correct_image_scaled)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 x, y = pygame.mouse.get_pos()
-                if ( (x >= 32) and (x <= 905) ) and ( (y >= 155) and (y <= 800) ):
+                if ((x >= 32) and (x <= 905)) and ((y >= 155) and (y <= 800)):
                     # left image clicked
-                    if stimuli[trial] == 1: # image on the left is correct => CORRECT CHOICE
+                    if stimuli[trial] == 1:  # image on the left is correct => CORRECT CHOICE
                         current_time = time.time()
                         latency_time = current_time - latency_start_time
-                        contents.append( [ str(trial+1), "\tLeft", "\tYes", '\t'+str(corrections), "\t\t"+str(latency_time)] )
+                        contents.append(
+                            [str(trial + 1), "\tLeft", "\tYes", "\t" + str(corrections), "\t\t" + str(latency_time)]
+                        )
                         trial += 1
                         corrections = 0
                         switch_arduino_on()
-                    else: # image on the left is wrong => INCORRECT CHOICE
+                    else:  # image on the left is wrong => INCORRECT CHOICE
                         current_time = time.time()
                         latency_time = current_time - latency_start_time
-                        contents.append( [ str(trial+1), "\tLeft", "\tNo", '\t'+str(corrections), "\t\t"+str(latency_time)] )
+                        contents.append(
+                            [str(trial + 1), "\tLeft", "\tNo", "\t" + str(corrections), "\t\t" + str(latency_time)]
+                        )
                         if corrections < 7:
                             corrections += 1
                         else:
                             trial += 1
                             corrections = 0
-                elif ( (x >= 952) and (x <= 1825) ) and ( (y >= 155) and (y <= 800) ):
+                elif ((x >= 952) and (x <= 1825)) and ((y >= 155) and (y <= 800)):
                     # right image clicked
-                    if stimuli[trial] == 0: # image on the right is correct => CORRECT CHOICE
+                    if stimuli[trial] == 0:  # image on the right is correct => CORRECT CHOICE
                         current_time = time.time()
                         latency_time = current_time - latency_start_time
-                        contents.append( [ str(trial+1), "\tRight", "\tYes", '\t'+str(corrections), "\t\t"+str(latency_time)] )
+                        contents.append(
+                            [str(trial + 1), "\tRight", "\tYes", "\t" + str(corrections), "\t\t" + str(latency_time)]
+                        )
                         trial += 1
                         corrections = 0
                         switch_arduino_on()
-                    else: # image on the right is wrong => INCORRECT CHOICE
+                    else:  # image on the right is wrong => INCORRECT CHOICE
                         current_time = time.time()
                         latency_time = current_time - latency_start_time
-                        contents.append( [ str(trial+1), "\tRight", "\tNo", '\t'+str(corrections), "\t\t"+str(latency_time)] )
+                        contents.append(
+                            [str(trial + 1), "\tRight", "\tNo", "\t" + str(corrections), "\t\t" + str(latency_time)]
+                        )
                         if corrections < 7:
                             corrections += 1
                         else:
@@ -135,11 +148,11 @@ def main():
 
     pygame.quit()
 
-
     with open("two_choice_correction.txt", "w") as f:
         for content in contents:
             f.writelines(content)
-            f.write('\n')
+            f.write("\n")
+
 
 if __name__ == "__main__":
     main()
