0c85a2acde84b8668252a576f1dcb5d07e0826d8
Michael Booth <michael@databooth.com.au>
2023-09-15 15:24:25 +1000

    progress

--- src/2Final_RPE_code.py
+++ src/2Final_RPE_code.py
@@ -1,103 +1,52 @@
-from datetime import datetime, timedelta
+from datetime import datetime
 import sys
 import time
 
-import pygame
-
 from experiment_helper import (
-    choose_session_type,
-    initialise_sensors,
-    initialise_directories,
-    load_validate_experimental_parameters,
-    set_subject_number,
-    confirm_experiment_details,
-    initialise_experiment,
+    setup_experiment,
     log_event,
-    log_trial_parameters,
-    initialise_GPIO,
-    Servo_grain,
-    Servo_pellets,
-    Button,
+    initialise_GPIO_buttons,
     play_WAV,
     pausable_sleep,
     elapsed_seconds,
     listenForPause,
+    Servo_grain,
+    Servo_pellets,
 )
 
-# NOTE new=Wav file paths- delete old paths
+import experiment_sounds as wav
 
-systemIsReadyWav = "/home/horselogic/Documents/test audio project_data/System_is_ready1.wav"
-sessionStartedWav = "/home/horselogic/Documents/test audio project_data/Session_started1.wav"
-trialPausedWav = "/home/horselogic/Documents/test audio project_data/Trial paused1.wav"
-trialRestartedWav = "/home/horselogic/Documents/test audio project_data/Trial restarted1.wav"
-sessionTerminated = "/home/horselogic/Documents/test audio project_data/End tone_1200_sess_completed.wav"
-startToneWav = "/home/horselogic/Documents/test audio project_data/Start tone _600.wav"
-correctToneWav = "/home/horselogic/Documents/test audio project_data/Correct tone_1000.wav"
-incorrectToneWav = "/home/horselogic/Documents/test audio project_data/IncorrectTone.wav"
-endToneWav = "/home/horselogic/Documents/test audio project_data/End tone_1200.wav"
-criterionAchievedWav = "//home/horselogic/Code/2Final_RPE_code_DRAFT.pyhome/horselogic/Documents/test audio project_data/Criterion_reached.wav"
-criterionNotReachedWav = "/home/horselogic/Documents/test audio project_data/Criterion_ not_reached.wav"
-acquisitionSessionStartedWav = "/home/horselogic/Documents/test audio project_data/Acquistion_Session_Started.wav"
-habitSessionStartedWav = "/home/horselogic/Documents/test audio project_data/Habit_session_started.wav"
-extinctionSessionStartedWav = "/home/horselogic/Documents/test audio project_data/Extinction_session_started.wav"
-reinstatementSessionStartedWav = "/home/horselogic/Documents/test audio project_data/Reinstatement_session_started.wav"
+#### Start of experiment ####
 
-
-# RP-A= acquisition of response
-# trialLimit = 20 per session, multiple sessions until acquisition criterion
-# unlimited time between start tone and touch (or button)
-# criterion = n touches in a row that occur under 20 sec from start tone
-# no responseTimeout
-# RP-H= habit formation
-# trialLimit 20 fixed trials per session, 3 sesions
-# reponseTimeout 45s
-# RP-E= extinction of response
-# trialLimit  = 20, multiple session until extinction criterion
-# number of responses in a row
-# criterion_seconds = 20s
-# RP-R= reinstatement of response
-# trialLimit = 20 per session, multiple sessions unil acquisition criterion
-# no responseTimeout
-
-# At beginning of each session for each horse- check and change session type to one of:
-# RP-A = acquisition,
-# RP-H = habit formation,
-# RP-E= extinction,
-# RP-R = reinstatement
+(
+    subject_name,
+    session_number,
+    session_type,
+    p,
+    touchSensor,
+    servo,
+    data_dir,
+    log_file,
+    measurement_file,
+) = setup_experiment()
 
 
 # Session settings
-trialLimit = 20  # all session types
+trialLimit = p["TRIAL_LIMIT"]  # all session types
 criterionLimit = 3  # check for session type-may differ between session types
 criterion_seconds = 20  # max duration in seconds for either a go or no-go response depending on session type
-responseTimeout = 45  # used for RP-H only- maximum time to respond to a start tone
+responseTimeout = p["RESPONSE_TIMEOUT"]  # used for RP-H only- maximum time to respond to a start tone
 
 # Define mode of operation - test or live
-trialSleepTime = 5
-mode = "test"
+trialSleepTime = p["TRIAL_SLEEP_TIME"]
+
+mode = p["MODE"]
+
 if mode == "test":
     trialLimit = 5
     criterion_seconds = 10
     trialSleepTime = 5  # sleep time period to allow feed consumption after a dispense
     responseTimeout = 10
-
-
-#### Start of experiment ####
-
-pygame.mixer.init()  # Initialise the mixer module for playing WAV files
-
-session_type = choose_session_type()
-data_dir, tone_dir = initialise_directories()
-p = load_validate_experimental_parameters(data_dir)
-
-touchSensor, servo = initialise_sensors(p["SENSITIVITY"], p["SERVO_CHANNEL"])
-subject_name, session_number = set_subject_number(p["N_TRIAL"], data_dir)
-
-if not confirm_experiment_details(subject_name, session_number, session_type, p["N_TRIAL"]):
-    sys.exit()
-log_file, measurement_file, initial_delay = initialise_experiment(subject_name, p["INITIAL_DELAY"], session_type)
-log_event(f"Subject {subject_name} - Session {session_number} started...", data_dir, log_file)
-log_trial_parameters(p, data_dir, log_file)
 
 # servo mode - change depending on which feed type is used
 servo_mode = p["SERVO_MODE"]
@@ -108,10 +57,8 @@ feeder_pellets = Servo_pellets(servo)
 
 
 # Initialise GPIO and create button instances
-gpio, butpin_green, butpin_blue, butpin_red = initialise_GPIO()
-green_button = Button(butpin_green)
-blue_button = Button(butpin_blue)
-red_button = Button(butpin_red)
+gpio, green_button, blue_button, red_button = initialise_GPIO_buttons()
+
 
 # Initialize variables
 trial_count = 0
@@ -133,10 +80,12 @@ try:
             green_button_press_count += 1
             if green_button_press_count == 1:
                 # pause to avoid button double-press
-                print(
-                    f"System is ready for {subject_name} {session_number} Session type {session_type} at: {datetime.now()}"
+                log_event(
+                    f"System is ready for {subject_name} {session_number} Session type {session_type} at: {datetime.now()}",
+                    data_dir,
+                    log_file,
                 )
-                play_WAV(systemIsReadyWav, p["SYSTEM_READY_SOUND_DURATION"])
+                play_WAV(wav.systemIsReady, p["SYSTEM_READY_SOUND_DURATION"])
 
             elif green_button_press_count == 2:
                 # pause to avoid button double-press
@@ -145,36 +94,37 @@ try:
                     f"Session started for {subject_name} {session_number} Session type {session_type} at: {datetime.now()}"
                 )
                 if session_type == "RP-A":
-                    play_WAV(acquisitionSessionStartedWav, p["SESSION_STARTED_SOUND_DURATION"])
+                    play_WAV(wav.acquisitionSessionStarted, p["SESSION_STARTED_SOUND_DURATION"])
                 elif session_type == "RP-H":
-                    play_WAV(habitSessionStartedWav, 1)
+                    play_WAV(wav.habitSessionStarted, p["SESSION_STARTED_SOUND_DURATION"])
                 elif session_type == "RP-E":
-                    play_WAV(extinctionSessionStartedWav, 1)
+                    play_WAV(wav.extinctionSessionStarted, p["SESSION_STARTED_SOUND_DURATION"])
                 elif session_type == "RP-R":
-                    play_WAV(reinstatementSessionStartedWav, 1)
+                    play_WAV(wav.reinstatementSessionStarted, p["SESSION_STARTED_SOUND_DURATION"])
                 else:
-                    play_WAV(sessionTerminated, 1)
+                    play_WAV(wav.sessionTerminated, p["SESSION_TERMINATED_SOUND_DURATION"])
                 pausable_sleep(0.5, False)
 
                 while trial_number < trialLimit:
                     if session_type == "RP-E":
                         if criterion_count == criterionLimit:
-                            play_WAV(criterionAchievedWav, 3)
+                            play_WAV(wav.criterionAchieved, 3)
                             print(f"Criterion reached at: {datetime.now()}")
                             sys.exit()
 
                     # begin next trial
                     trial_number += 1
                     print(f"Current trial: {trial_number}")
-                    play_WAV(startToneWav, 1)
+                    play_WAV(wav.startTone, 1)
                     start_tone_played = True
                     start_tone_time = datetime.now()
-                    print(f"Start tone played at: {start_tone_time}")
+                    # print(f"Start tone played at: {start_tone_time}")
+                    log_event("Start tone played", data_dir, log_file)
 
                     # waiting for either a touch or the blue button
                     while True:
                         start_tone_time = start_tone_time + listenForPause(
-                            red_button, False, blue_button, touchSensor, trialPausedWav, trialRestartedWav
+                            red_button, False, blue_button, touchSensor, trialPaused, trialRestarted
                         )
 
                         # elapsed = elapsed_seconds(start_tone_time)
@@ -184,13 +134,14 @@ try:
                         if session_type == "RP-H":
                             if elapsed_seconds(start_tone_time) > responseTimeout:
                                 print(f"Habit formation incorrect response at {datetime.now()}")
-                                play_WAV(incorrectToneWav, 1)
+                                play_WAV(wav.incorrectTone, 1)
                                 start_tone_time = start_tone_time + pausable_sleep(1, False)
                                 break
                         elif session_type == "RP-E":
                             if elapsed_seconds(start_tone_time) > criterion_seconds:
                                 criterion_count += 1
-                                print(f"Criterion count {criterion_count}")
+                                # print(f"Criterion count {criterion_count}")
+                                log_event(f"Criterion count {criterion_count}", data_dir, log_file)
                                 break
 
                         if blue_button.is_pressed():
@@ -199,7 +150,8 @@ try:
                                 is_touch_active = False
                                 last_touch_time = datetime.now()
                                 touch_count += 1
-                                print(f"Manual touch recorded at: {last_touch_time}, Session under manual control")
+                                # print(f"Manual touch recorded at: {last_touch_time}, Session under manual control")
+                                log_event("Manual touch recorded - Session under manual control")
 
                                 # assess goal
                                 if session_type == "RP-A" or session_type == "RP-R":
@@ -216,7 +168,7 @@ try:
                                     break
 
                                 time.sleep(0.1)
-                                play_WAV(correctToneWav, 1)
+                                play_WAV(wav.correctTone, 1)
                                 time.sleep(0.5)
                                 if servo_mode == "pellets":
                                     feeder_pellets.dispense_feed_pellets()
@@ -224,12 +176,12 @@ try:
                                     feeder_grain.dispense_feed_grain()
 
                                 if criterion_count == criterionLimit:
-                                    play_WAV(criterionAchievedWav, 3)
+                                    play_WAV(wav.criterionAchieved, 3)
                                     print(f"Criterion reached at: {datetime.now()}")
                                     sys.exit()
 
                                 if touch_count == trialLimit:
-                                    play_WAV(sessionTerminated, 3.5)
+                                    play_WAV(wav.sessionTerminated, 3.5)
                                     print(f"Session ended at: {datetime.now()}")
                                     sys.exit()
 
@@ -247,7 +199,8 @@ try:
                             if status[1] > 0 or status[2] > 0 or status[3] > 0:
                                 last_touch_time = datetime.now()
                                 touch_count += 1
-                                print(f"Touch-pad status read at: {last_touch_time}")
+                                # print(f"Touch-pad status read at: {last_touch_time}")
+                                log_event("Touch-pad status read", data_dir, log_file)
                                 time.sleep(0.01)
 
                                 if session_type == "RP-A" or session_type == "RP-R":
@@ -263,7 +216,7 @@ try:
                                     start_tone_time = start_tone_time + pausable_sleep(trialSleepTime, True)
                                     break
 
-                                play_WAV(correctToneWav, 1)
+                                play_WAV(wav.correctTone, 1)
                                 time.sleep(0.5)
                                 if servo_mode == "pellets":
                                     feeder_pellets.dispense_feed_pellets()
@@ -271,12 +224,12 @@ try:
                                     feeder_grain.dispense_feed_grain()
 
                                 if criterion_count == criterionLimit:
-                                    play_WAV(criterionAchievedWav, 3)
+                                    play_WAV(wav.criterionAchieved, 3)
                                     print(f"Criterion achieved at: {datetime.now()}")
                                     sys.exit()
 
                                 if touch_count == trialLimit:
-                                    play_WAV(sessionTerminated, 3.5)
+                                    play_WAV(wav.sessionTerminated, 3.5)
                                     print(f"Session ended at: {datetime.now()}")
                                     sys.exit()
 
@@ -284,7 +237,7 @@ try:
                                 break
 
                 if trial_number == trialLimit:
-                    play_WAV(sessionTerminated, 3.5)
+                    play_WAV(wav.sessionTerminated, 3.5)
                     sys.exit()
 
 except KeyboardInterrupt:
