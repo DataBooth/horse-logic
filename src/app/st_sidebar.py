cf44a9638fab2a6618a5e3937859a84edbea1ea0
Michael Booth <michael@databooth.com.au>
2023-09-19 11:36:48 +1000

    cleanup

--- src/app/st_sidebar.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-
-def create_sidebar():
-    st.sidebar.markdown("## TODO: Create a sidebar")
-    return None
--- src/pages/9_📂_Source_Code.py
+++
@@ -1,3 +0,0 @@
-from st_helper import read_render_markdown_file
-
-read_render_markdown_file("docs/app_github.md")
